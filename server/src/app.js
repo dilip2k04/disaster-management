c65fcb15b10660cc3107a05b18be4853899fa68b
Dilip <162571689+dilip2k04@users.noreply.github.com>
2026-02-01 15:00:26 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-frontend.onrender.com"
+    "https://disaster-management-1-gwmm.onrender.com"
   ],
   credentials: true
 }));
